4adf42f2a842af0807e6e0b65d0a2fa17d138239
Jeff Zohrab <jzohrab@gmail.com>
2021-01-09 14:47:50 -0800

    Use new scanning method for questions.

--- lib/AudioClozeHelpers.rb
+++ lib/AudioClozeHelpers.rb
@@ -12,17 +12,19 @@ class AudioClozeHelpers
       return text
     end
 
+    # :scan tip from
+    # https://stackoverflow.com/questions/80357/
+    #   how-to-match-all-occurrences-of-a-regex
     ms = text.to_enum(:scan, clozeRe).map { Regexp.last_match }
-    # puts ms.inspect
-    # ms.each do |m|
-    #   puts m[0]
-    #   puts m[:answer]
-    #   puts m[:hint]
-    # end
+
+    question = text
+    ms.each do |m|
+      a = m[:answer]
+      blanks = a.gsub(/\w+/, '___')
+      question = question.gsub(m[0], blanks)
+    end
 
     hints = ms.map { |m| m[:hint] }.select { |h| !h.nil? }
-
-    question = text.gsub(clozeRe, '___')
     if (hints.size > 0) then
       question = "#{hints.join(', ')}.  #{question}"
     end
@@ -51,6 +53,7 @@ if __FILE__ == $0
       puts q
       raise q
     end
+    puts q
   end
 
   def check_a(text, expected)
@@ -60,20 +63,23 @@ if __FILE__ == $0
       puts a
       raise a
     end
+    puts a
   end
 
-  check_q("A *q1|h1* ok.", "h1.  A ___ ok.")
-  check_q("B *q1* ok.", "B ___ ok.")
-  # check_q("C *q1a q1b|h1* ok.", "h1.  C ___ ___ ok.")
-  # check_q("D *q1b q1b* ok.", "D ___ ___ ok.")
-  check_q("E q1 ok.", "E q1 ok.")
-  check_q("F *q1|h1*, *q2|h2* *q3* ok.", "h1, h2.  F ___, ___ ___ ok.")
-  # check_q("G *q1|h1*, *q2* *q3 q3b|h3* ok.", "h1, h3.  G ___, ___ ___ ___ ok.")
+  check_q("qA *q1|h1* ok.", "h1.  qA ___ ok.")
+  check_q("qB *q1* ok.", "qB ___ ok.")
+  check_q("qC *q1a q1b|h1* ok.", "h1.  qC ___ ___ ok.")
+  check_q("qD *q1b q1b* ok.", "qD ___ ___ ok.")
+  check_q("qE q1 ok.", "qE q1 ok.")
+  check_q("qF *q1|h1*, *q2|h2* *q3* ok.", "h1, h2.  qF ___, ___ ___ ok.")
+  check_q("qG *q1|h1*, *q2* *q3 q3b|h3* ok.", "h1, h3.  qG ___, ___ ___ ___ ok.")
 
-  check_a("A *q1|h1* ok.", "A q1 ok.")
-  check_a("B *q1* ok.", "B q1 ok.")
-  check_a("C q1 ok.", "C q1 ok.")
-  check_a("D *q1|h1*, *q2|h2* ok.", "D q1, q2 ok.")
-  check_a("E *q1|h1*, *q2* *q3|h3* ok.", "E q1, q2 q3 ok.")
-  check_a("F *q1 q1b|h1*, *q2 q2b* *q3 q3b|h3* ok.", "F q1 q1b, q2 q2b q3 q3b ok.")
+  check_a("aA *q1|h1* ok.", "aA q1 ok.")
+  check_a("aB *q1* ok.", "aB q1 ok.")
+  check_a("aC q1 ok.", "aC q1 ok.")
+  check_a("aD *q1|h1*, *q2|h2* ok.", "aD q1, q2 ok.")
+  check_a("aE *q1|h1*, *q2* *q3|h3* ok.", "aE q1, q2 q3 ok.")
+  check_a("aF *q1 q1b|h1*, *q2 q2b* *q3 q3b|h3* ok.", "aF q1 q1b, q2 q2b q3 q3b ok.")
+
+  puts "OK."
 end
