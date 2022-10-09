dcd269bb8a72ba78627e955bb6df25567f989edf
Jeff Zohrab <jzohrab@gmail.com>
2022-10-09 11:57:45 -0700

    Planned behaviour.

--- test/tc_audio_cloze_helpers.rb
+++ test/tc_audio_cloze_helpers.rb
@@ -33,6 +33,11 @@ class TestAudioClozeHelpers < Test::Unit::TestCase
     check_a("aI [check ßäüö|h1] here.", "aI check ßäüö here.")
   end
 
+  def TODO_test_hint_with_detail()
+    check_q("start [word|hint|detail] ok.", "hint. start ___ ok.")
+    check_a("start [word|hint|detail] ok.", "detail. start word ok.")
+  end
+
   # Sometimes it's good to 'fake' cloze something ...
   def test_empty_cloze()
     check_q('X prefieren [] caminar.', 'X prefieren ___ caminar.')
